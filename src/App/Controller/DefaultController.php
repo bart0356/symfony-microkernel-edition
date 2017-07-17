c02054476112c6cea61e8fbc532ab9af6deaf958
Laurent WILLEMS <webmaster@comptoir-des-monnaies.com>
2017-07-17 17:25:42 +0200

    Symfony 3.3 autowire & autoconfigure

--- src/App/Controller/DefaultController.php
+++ src/App/Controller/DefaultController.php
@@ -4,16 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction()
+    public function indexAction(\Twig_Environment $twig)
     {
-        return $this->render('default/index.html.twig', [
+        $content = $twig->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
+
+        return new Response($content);
     }
 }
